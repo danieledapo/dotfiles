172276332a1deb29288b859cfc6f5e9334b7aeec
Daniele D'Orazio <d.dorazio96@gmail.com>
2020-11-04 16:32:04 +0100

    config.fish: use source instead of .

--- fish/config.fish
+++ fish/config.fish
@@ -45,9 +45,6 @@ if test -d $HOME/.pyenv/bin
     set -x PYENV_ROOT "$HOME/.pyenv"
     set -x PATH "$PYENV_ROOT/bin" $PATH
 
-    status --is-interactive
-    and . (pyenv init -|psub)
-
-    status --is-interactive
-    and . (pyenv virtualenv-init -|psub)
+    status --is-interactive; and source (pyenv init -|psub)
+    status --is-interactive; and source (pyenv virtualenv-init -|psub)
 end
