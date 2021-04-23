6b36f9e10871a698b3a76f7d3690a9d2f3c125ac
Daniele D'Orazio <d.dorazio96@gmail.com>
2021-04-23 14:13:10 +0200

    fish: remove old autojump hook

--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,6 @@ set -x PATH "$HOME/.local/bin" "$HOME/.cargo/bin" $PATH
 #
 # Autojump
 #
-if test -f /usr/local/share/autojump/autojump.fish
-    source /usr/local/share/autojump/autojump.fish
-end
 
 if test -f /usr/share/autojump/autojump.fish
     source /usr/share/autojump/autojump.fish
