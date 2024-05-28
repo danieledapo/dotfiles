057f0c8404b72c5a99639dbb80ac5192e598f675
Daniele D'Orazio <d.dorazio96@gmail.com>
2024-05-28 14:22:33 +0200

    nvim: fewer dependencies by default

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,14 +1,9 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "black",
-      "ruff-lsp",
-      "rust-analyzer",
-      "shellcheck",
-      "shfmt",
-      "stylua",
-      "typescript-language-server",
-    },
-  },
+	"williamboman/mason.nvim",
+	opts = {
+		ensure_installed = {
+			"rust-analyzer",
+			"shellcheck",
+		},
+	},
 }
