50ba62d081a8f28f0a03f078b70f93031cd38221
Daniele D'Orazio <d.dorazio96@gmail.com>
2025-07-31 17:48:03 +0200

    nvim: cosmetics

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,11 +1,11 @@
 return {
-	"folke/snacks.nvim",
-	opts = {
-		zen = {
-			toggles = {
-				dim = false,
-				diagnostics = true,
-			},
-		},
-	},
+  "folke/snacks.nvim",
+  opts = {
+    zen = {
+      toggles = {
+        dim = false,
+        diagnostics = true,
+      },
+    },
+  },
 }
